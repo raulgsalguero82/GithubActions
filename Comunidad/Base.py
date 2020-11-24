1f3ab3a281f17bb309e1a79a1f17efdcd20a74a7
Raul Garcia Salguero <r.garcia3@uniandes.edu.co>
2020-11-24 17:59:05 -0500

    *Verify DB directory

--- Comunidad/Base.py
+++ Comunidad/Base.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('sqlite:///PersonasDB.db')
+engine = create_engine('sqlite:///Resources/PersonasDB.db')
 Session = sessionmaker(bind=engine)
 
 Base = declarative_base()
